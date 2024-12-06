fdc4cea75b1ea57e7703eabc94a0ebdec4197633
AsePlayer <52973701+AsePlayer@users.noreply.github.com>
2024-12-05 20:44:09 -0800

    Added hyperparamaters

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import torch
+from torch.nn import functional as F
 from models.bigramLanguageModel import BigramLanguageModel
+
+# hyperparameters
+batch_size = 16 # how many independent sequences will we process in parallel?
+block_size = 32 # what is the maximum context length for predictions?
+max_iters = 5000
+eval_interval = 100
+learning_rate = 1e-3
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
+n_embd = 64
+n_head = 4
+n_layer = 4
+dropout = 0.0
+# ------------
 
 '''Step 1: Read the data'''
 # Open the file 'input.txt' and read its content into a variable called `text`.
